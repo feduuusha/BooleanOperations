7e9734fa97f179fce029296705f85584926adffa
Fyodor Voropaev <voropaevfedor2005@mail.ru>
2023-10-12 22:18:40 +0300

    add SCNF and SDNF functions

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         StringConverter gen = new StringConverter("(!x1 || x2) && x3");
-
-        gen.output();
+        gen.table();
+        gen.scnf();
+        gen.sdnf();
     }
 
 }
--- src/StringConverter.java
+++ src/StringConverter.java
@@ -67,10 +67,37 @@ public class StringConverter {
 
         }
     }
-    public void output() {
+    public void table() {
         for (int i = 0; i < numberOfVars; ++i) System.out.print("x" + (i+1) + " ");
         System.out.print(" F\n");
         for (int[] ints : table) System.out.println(Arrays.toString(ints));
     }
-
+    public void scnf() {
+        char neg = '¬';
+        for (int[] variable : table) {
+            if (variable[variable.length - 1] == 0) {
+                System.out.print("(");
+                for (int j = 0; j < variable.length - 1; ++j) {
+                    boolean item = variable[j] == 1;
+                    System.out.print((item ? neg + "x" + (j + 1) : "x" + (j + 1)) + " ∧ ");
+                }
+                System.out.print("\b\b\b)∨");
+            }
+        }
+        System.out.println("\b");
+    }
+    public void sdnf() {
+        char neg = '¬';
+        for (int[] variable : table) {
+            if (variable[variable.length - 1] == 1) {
+                System.out.print("(");
+                for (int j = 0; j < variable.length - 1; ++j) {
+                    boolean item = variable[j] == 1;
+                    System.out.print((item ? "x" + (j + 1) : neg + "x" + (j + 1)) + " ∨ ");
+                }
+                System.out.print("\b\b\b)∧");
+            }
+        }
+        System.out.println("\b");
+    }
 }
