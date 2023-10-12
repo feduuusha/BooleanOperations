77c7b89fcd61f636b9194ab3eddf6a41b32ba1ec
Fyodor Voropaev <voropaevfedor2005@mail.ru>
2023-10-12 23:54:46 +0300

    add beta(hardcore) version of function abbreviatedDNF

--- src/StringConverter.java
+++ src/StringConverter.java
@@ -9,22 +9,22 @@ public class StringConverter {
     int[][] table;
     public StringConverter(String str){
         this.stringLine = str;
-        this.counterParametrs();
+        this.counterParameters();
         this.createTable();
         this.defaultFiller();
         this.converter();
         this.fillTableF();
     }
-    public void counterParametrs() {
+    private void counterParameters() {
         int counter = 0;
         while (stringLine.contains("x" + (counter + 1))) ++counter;
         this.numberOfVars = counter;
         for (int i = 0; i < numberOfVars; ++i) this.rows *= 2;
     }
-    public void createTable() {
+    private void createTable() {
         this.table = new int[rows][numberOfVars + 1];
     }
-    public void defaultFiller() {
+    private void defaultFiller() {
         int x = rows, col = 0;
         while (x != 1) {
             x /= 2;
@@ -38,7 +38,7 @@ public class StringConverter {
 
         }
     }
-    public void converter() {
+    private void converter() {
         String expression = "if (";
 //        for (int i = 0; i < stringLine.length(); i++) {
 //
@@ -47,7 +47,7 @@ public class StringConverter {
         expression += ") res = 1;";
         this.expression = expression;
     }
-    public void fillTableF() {
+    private void fillTableF() {
         for (int i = 0; i < table.length; ++i) {
             Interpreter inter = new Interpreter();
             for (int j = 1; j <= table[0].length; ++j) {
@@ -99,5 +99,59 @@ public class StringConverter {
             }
         }
         System.out.println("\b");
+    }
+    public void abbreviatedDNF() {
+        switch (numberOfVars) {
+            case 2:
+                for (int i = 0; i < numberOfVars; ++i) {
+                    int flag = 1, negFlag = 1;
+                    for (int[] row : table) {
+                        if (row[i] == 1) {
+                            if (row[row.length - 1] == 0) {
+                                flag *= 0;
+                            }
+                        } else {
+                            if (row[row.length - 1] == 0) {
+                                negFlag *= 0;
+                            }
+                        }
+                    }
+                    boolean booFlag = flag == 1;
+                    boolean booNegFlag = negFlag == 1;
+                    System.out.print((booFlag ? "x" + (i + 1) : "") + (booNegFlag ? "¬x" + (i + 1) : ""));
+                }
+            case 3:
+                for (int j = 0; j < numberOfVars; ++j) {
+                    for (int i = j + 1; i < numberOfVars; ++i) {
+                        int flag1 = 1, flag2 = 1, flag3 = 1, flag4 = 1;
+                        for (int[] row : table) {
+                            if (row[i] == 1 && row[j] == 1) {
+                                if (row[row.length - 1] == 0) {
+                                    flag1 *= 0;
+                                }
+                            } else if (row[i] == 1 && row[j] == 0) {
+                                if (row[row.length - 1] == 0) {
+                                    flag2 *= 0;
+                                }
+                            } else if (row[i] == 0 && row[j] == 1) {
+                                if (row[row.length - 1] == 0) {
+                                    flag3 *= 0;
+                                }
+                            } else {
+                                if (row[row.length - 1] == 0) {
+                                    flag4 *= 0;
+                                }
+                            }
+                        }
+                        System.out.print((flag1==1?"(x" + (j+1) + " ^ x" + (i+1) + ")":"") +
+                                            (flag2==1?"(¬x" + (j+1) + " ^ x" + (i+1) + ")":"") +
+                                            (flag3==1?"(x" + (j+1) + " ^ ¬x" + (i+1) + ")":"") +
+                                            (flag4==1?"(¬x" + (j+1) + " ^ ¬x" + (i+1) + ")":""));
+                    }
+
+                }
+
+
+        }
     }
 }
