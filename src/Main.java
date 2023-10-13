8ce2cd7d6ecc0fc65fb5dc6320a81ea863d29a4d
Fyodor Voropaev <voropaevfedor2005@mail.ru>
2023-10-13 11:20:57 +0300

    reworked the abbrDNF function, but it seems to work incorrectly

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        StringConverter gen = new StringConverter("(!x1 && !x2 && x3) || (!x1 && x2 && !x3) || (!x1 && x2 && x3) || (x1 && !x2 && !x3) || (x1 && !x2 && x3)");
+        StringConverter gen = new StringConverter("(!x1 && !x2 && x3) || (!x1 && x2 && !x3) || (!x1 && x2 && x3) || (x1 && !x2 && !x3) || (x1 && x2 && x3)");
         gen.table();
         gen.scnf();
         gen.sdnf();
-        gen.abbreviatedDNF();
+        gen.abbrDNF();
     }
 }
--- src/StringConverter.java
+++ src/StringConverter.java
@@ -1,14 +1,15 @@
 import bsh.EvalError;
 import bsh.Interpreter;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class StringConverter {
-    String stringLine, expression;
+    String stringInput, expression;
     int numberOfVars, rows = 1;
     int[][] table;
-    public StringConverter(String str){
-        this.stringLine = str;
+    private final List<List<Integer>> abbrDNF = new ArrayList<>();
+    public StringConverter(String str) {
+        this.stringInput = str;
         this.counterParameters();
         this.createTable();
         this.defaultFiller();
@@ -17,7 +18,7 @@ public class StringConverter {
     }
     private void counterParameters() {
         int counter = 0;
-        while (stringLine.contains("x" + (counter + 1))) ++counter;
+        while (stringInput.contains("x" + (counter + 1))) ++counter;
         this.numberOfVars = counter;
         for (int i = 0; i < numberOfVars; ++i) this.rows *= 2;
     }
@@ -30,7 +31,7 @@ public class StringConverter {
             x /= 2;
             boolean res = true;
             for (int i = 0; i < table.length; ++i) {
-                if (i % x == 0 ) res = !res;
+                if (i % x == 0) res = !res;
                 table[i][col] = res ? 1 : 0;
             }
             col += 1;
@@ -38,12 +39,13 @@ public class StringConverter {
 
         }
     }
+
     private void converter() {
         String expression = "if (";
 //        for (int i = 0; i < stringLine.length(); i++) {
 //
 //        }
-        expression += stringLine;
+        expression += stringInput;
         expression += ") res = 1;";
         this.expression = expression;
     }
@@ -60,15 +62,24 @@ public class StringConverter {
             try {
                 inter.set("res", 0);
                 inter.eval(expression);
-                table[i][(table[i].length)-1] = (int) inter.get("res");
+                table[i][(table[i].length) - 1] = (int) inter.get("res");
             } catch (EvalError ex) {
                 System.out.println(ex.getMessage());
             }
-
+        }
+    }
+    private void fillListSDNF() {
+        for (int[] row : table) {
+            if (row[row.length - 1] == 1) {
+                abbrDNF.add(new ArrayList<>());
+                for (int j = 0; j < row.length - 1; ++j) {
+                    abbrDNF.get(abbrDNF.size() - 1).add(row[j]);
+                }
+            }
         }
     }
     public void table() {
-        for (int i = 0; i < numberOfVars; ++i) System.out.print("x" + (i+1) + " ");
+        for (int i = 0; i < numberOfVars; ++i) System.out.print("x" + (i + 1) + " ");
         System.out.print(" F\n");
         for (int[] ints : table) System.out.println(Arrays.toString(ints));
     }
@@ -100,57 +111,56 @@ public class StringConverter {
         }
         System.out.println("\b");
     }
-    public void abbreviatedDNF() {
-        switch (numberOfVars) {
-            case 2:
-                for (int i = 0; i < numberOfVars; ++i) {
-                    int flag = 1, negFlag = 1;
-                    for (int[] row : table) {
-                        if (row[i] == 1) {
-                            if (row[row.length - 1] == 0) {
-                                flag *= 0;
-                            }
-                        } else {
-                            if (row[row.length - 1] == 0) {
-                                negFlag *= 0;
-                            }
-                        }
-                    }
-                    boolean booFlag = flag == 1;
-                    boolean booNegFlag = negFlag == 1;
-                    System.out.print((booFlag ? "x" + (i + 1) : "") + (booNegFlag ? "¬x" + (i + 1) : ""));
-                }
-            case 3:
-                for (int j = 0; j < numberOfVars; ++j) {
-                    for (int i = j + 1; i < numberOfVars; ++i) {
-                        int flag1 = 1, flag2 = 1, flag3 = 1, flag4 = 1;
-                        for (int[] row : table) {
-                            if (row[i] == 1 && row[j] == 1) {
-                                if (row[row.length - 1] == 0) {
-                                    flag1 *= 0;
-                                }
-                            } else if (row[i] == 1 && row[j] == 0) {
-                                if (row[row.length - 1] == 0) {
-                                    flag2 *= 0;
-                                }
-                            } else if (row[i] == 0 && row[j] == 1) {
-                                if (row[row.length - 1] == 0) {
-                                    flag3 *= 0;
-                                }
-                            } else {
-                                if (row[row.length - 1] == 0) {
-                                    flag4 *= 0;
+    private void abbreviatedDNF() {
+        int counter;
+        do {
+            List<List<Integer>> clone = new ArrayList<>(abbrDNF);
+            List<Integer> removable = new ArrayList<>();
+            int count = 0;
+            for (int k = 0; k < numberOfVars; ++k) {
+                for (int j = 0; j < abbrDNF.size(); ++j) {
+                    for (int i = j + 1; i < abbrDNF.size(); ++i) {
+                        if (abbrDNF.get(j).size() == abbrDNF.get(i).size()) {
+                            if (k < abbrDNF.get(j).size()) {
+                                if (!abbrDNF.get(j).get(k).equals(abbrDNF.get(i).get(k))) {
+                                    List<Integer> lst1 = new ArrayList<>(abbrDNF.get(j));
+                                    List<Integer> lst2 = new ArrayList<>(abbrDNF.get(i));
+                                    lst1.set(k, -1);
+                                    lst2.set(k, -1);
+                                    if (lst1.equals(lst2)) {
+                                        clone.add(lst1);
+                                        count++;
+                                        removable.add(i);
+                                        removable.add(j);
+                                    }
                                 }
                             }
                         }
-                        System.out.print((flag1==1?"(x" + (j+1) + " ^ x" + (i+1) + ")":"") +
-                                            (flag2==1?"(¬x" + (j+1) + " ^ x" + (i+1) + ")":"") +
-                                            (flag3==1?"(x" + (j+1) + " ^ ¬x" + (i+1) + ")":"") +
-                                            (flag4==1?"(¬x" + (j+1) + " ^ ¬x" + (i+1) + ")":""));
                     }
-
                 }
-
+            }
+            Set<Integer> set = new HashSet<>(removable);
+            removable.clear();
+            removable.addAll(set);
+            Collections.sort(removable);
+            Collections.reverse(removable);
+            for (int var : removable) clone.remove(var);
+            abbrDNF.clear();
+            abbrDNF.addAll(clone);
+            counter = count;
+        }
+        while (counter != 0);
+    }
+    public void abbrDNF() {
+        this.fillListSDNF();
+        abbreviatedDNF();
+        for (List<Integer> conjunction : abbrDNF) {
+            System.out.print("(");
+            for (int j = 0; j < conjunction.size(); ++j) {
+                Integer el = conjunction.get(j);
+                if (el != -1) System.out.print((el == 1 ? "x" + (j + 1) : "¬x" + (j + 1)) + " ^ ");
+            }
+            System.out.print("\b\b\b) ");
 
         }
     }
